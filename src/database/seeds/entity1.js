6387212c18c4cee602cf8855ec567176c1f0d7b8
Alex Ahn <accounts+technology+github@work.alexahn.com>
2024-04-18 12:45:53 -0700

    wip

---
+++ src/database/seeds/entity1.js
@@ -0,0 +1,26 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> } 
+ */
+const seed = async function(knex) {
+  // Deletes ALL existing entries
+  await knex('gauze__entity1').del()
+  await knex('gauze__entity1').insert([
+    {
+		id: 1,
+		created_at: new Date(),
+		updated_at: new Date(),
+		text: 'hello'
+	},
+    {
+		id: 2,
+		created_at: new Date(),
+		updated_at: new Date(),
+		text: 'goodbye'
+	},
+  ]);
+};
+
+export {
+	seed 
+}
---
+++ src/database/seeds/entity2.js
@@ -0,0 +1,26 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> } 
+ */
+const seed = async function(knex) {
+  // Deletes ALL existing entries
+  await knex('gauze__entity2').del()
+  await knex('gauze__entity2').insert([
+    {
+        id: 1,
+        created_at: new Date(),
+        updated_at: new Date(),
+        text: 'begin'
+    },
+    {
+        id: 2,
+        created_at: new Date(),
+        updated_at: new Date(),
+        text: 'end'
+    },
+  ]);
+};
+
+export {
+	seed
+}
