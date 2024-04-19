628d0f602d6574ca815ae0462ab52e2d0203cd66
Alex Ahn <accounts+technology+github@work.alexahn.com>
2024-04-19 09:03:01 -0700

    add relationship model - one as a migration - another as a static file to bootstrap a new project

---
+++ src/database/relationship.js
@@ -0,0 +1,39 @@
+/**
+	* @param { import("knex").Knex } knex
+	* @returns { Promise<void> }
+	*/
+const up = function (knex) {
+	return Promise.all([
+		knex.schema.createTable('gauze__relationship', function (table) {
+			table.increments('id').primary();
+			table.date('created_at').notNullable();
+			table.date('updated_at').notNullable();
+			table.date('deleted_at');
+			// change to uuid later
+			table.bigint('from_id').notNullable();
+			table.string('from').notNullable();
+			// change to uuid later
+			table.bigint('to_id').notNullable();
+			table.string('to').notNullable();
+			table.index('from_id');
+			table.index('from');
+			table.index('to_id');
+			table.index('to');
+		})
+	]);
+};
+
+/**
+	* @param { import("knex").Knex } knex
+	* @returns { Promise<void> }
+	*/
+const down = function (knex) {
+	return Promise.all([
+		knex.schema.dropTable('gauze__relationship')
+	]);
+};
+
+export {
+	up,
+	down
+}
