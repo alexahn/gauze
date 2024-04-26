103f6248446310c586c73dfb0e4116ac4eb88437
Alex Ahn <accounts+technology+github@work.alexahn.com>
2024-04-25 18:52:19 -0700

    create and delete working

--- src/command/gauze.js
+++ src/command/gauze.js
@@ -4,9 +4,10 @@ import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
 
 import * as create from "./commands/create/index.js";
+import * as del from "./commands/delete/index.js";
 import * as run from "./commands/run/index.js";
 import * as project from "./commands/project.js";
 import * as migrate from "./commands/migrate/index.js";
 
-yargs(hideBin(process.argv)).command(create).command(run).command(migrate).command(project).demandCommand().help().parse();
+yargs(hideBin(process.argv)).command(create).command(del).command(run).command(migrate).command(project).demandCommand().help().parse();
 //.wrap(128)
