6e59727b4b8bd9bb1d185a06214e6b3fb58e3623
Alex Ahn <accounts+technology+github@work.alexahn.com>
2024-04-27 16:42:26 -0700

    prepare to add blacklist

--- src/kernel/models/class.js
+++ src/kernel/models/class.js
@@ -17,7 +17,7 @@ class Model {
 			this.relationship_table = $structure.relationship.database.sql.TABLE_NAME__SQL__DATABASE__RELATIONSHIP__STRUCTURE;
 		} else {
 			this.relationship_table = "";
-			$kernel.logger.io.LOGGER__IO__LOGGER__KERNEL.write("6", __RELATIVE_FILEPATH, `${this.name}.constructor:FATAL`, new Error("Relationship structure not found"));
+			$kernel.logger.io.LOGGER__IO__LOGGER__KERNEL.write("5", __RELATIVE_FILEPATH, `${this.name}.constructor:WARNING`, new Error("Relationship structure not found"));
 		}
 		$kernel.logger.io.LOGGER__IO__LOGGER__KERNEL.write("0", __RELATIVE_FILEPATH, `${this.name}.constructor:exit`);
 	}
