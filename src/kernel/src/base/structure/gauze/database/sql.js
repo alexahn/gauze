b69d6fb5c4fe7183fd7983eaa6ceddb5478bb98d
Alex Ahn <accounts+technology+github@work.alexahn.com>
2024-04-26 20:06:34 -0700

    remake kernel base

--- src/kernel/src/base/structure/gauze/database/sql.js
+++ src/kernel/src/base/structure/gauze/database/sql.js
@@ -2,7 +2,7 @@ import * as $abstract from "./../../../abstract/index.js";
 
 import { SERIALIZER_CREATED_AT_STRUCTURE, SERIALIZER_UPDATED_AT_STRUCTURE, SERIALIZER_DELETED_AT_STRUCTURE } from "./../../serializers.js";
 
-const TABLE_NAME__SQL__DATABASE__caf5342ac38d41a6a02bb81d2d2b21a4__STRUCTURE = "a543731262804f64adcc0eae1a225acc";
+const TABLE_NAME__SQL__DATABASE__caf5342ac38d41a6a02bb81d2d2b21a4__STRUCTURE = "gauze__a543731262804f64adcc0eae1a225acc";
 const PRIMARY_KEY__SQL__DATABASE__caf5342ac38d41a6a02bb81d2d2b21a4__STRUCTURE = $abstract.entities.a543731262804f64adcc0eae1a225acc.default($abstract).primary_key;
 
 export { TABLE_NAME__SQL__DATABASE__caf5342ac38d41a6a02bb81d2d2b21a4__STRUCTURE, PRIMARY_KEY__SQL__DATABASE__caf5342ac38d41a6a02bb81d2d2b21a4__STRUCTURE };
