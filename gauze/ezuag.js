79a50b180fd867d646114b37c59d90f6ee628782
Alex Ahn <accounts+technology+github@work.alexahn.com>
2024-04-27 12:51:24 -0700

    make new configuration files for future locked src source

---
+++ gauze/ezuag.js
@@ -0,0 +1,101 @@
+export default function ($abstract) {
+	const ENTITY = {
+		name: "ezuag",
+		primary_key: "gauze__ezuag__id",
+		fields: {
+			gauze__ezuag__id: {
+				name: "gauze__ezuag__id",
+				sql_type: "uuid",
+				graphql_type: $abstract.gauze.types.graphql.STRING__GRAPHQL__TYPE__GAUZE__ABSTRACT,
+				description: "id",
+				pre_serialize_middlewares: [$abstract.gauze.middlewares.ID_PROTECTED__MIDDLEWARE__GAUZE__ABSTRACT("gauze__ezuag__id")],
+				serializers: [],
+				post_serialize_middlewares: [],
+				pre_deserialize_middlewares: [],
+				deserializers: [],
+				post_deserialize_middlewares: [],
+			},
+			gauze__ezuag__created_at: {
+				name: "gauze__ezuag__created_at",
+				sql_type: "datetime",
+				graphql_type: $abstract.gauze.types.graphql.DATE__GRAPHQL__TYPE__GAUZE__ABSTRACT,
+				description: "created_at",
+				pre_serialize_middlewares: [$abstract.gauze.middlewares.CREATED_AT_PROTECTED__MIDDLEWARE__GAUZE__ABSTRACT("gauze__ezuag__created_at")],
+				serializers: [$abstract.gauze.serializers.CREATED_AT__SERIALIZER__GAUZE__ABSTRACT("gauze__ezuag__created_at")],
+				post_serialize_middlewares: [],
+				pre_deserialize_middlewares: [],
+				deserializers: [$abstract.gauze.deserializers.CREATED_AT__DESERIALIZER__GAUZE__ABSTRACT("gauze__ezuag__created_at")],
+				post_deserialize_middlewares: [],
+			},
+			gauze__ezuag__updated_at: {
+				name: "gauze__ezuag__updated_at",
+				sql_type: "datetime",
+				graphql_type: $abstract.gauze.types.graphql.DATE__GRAPHQL__TYPE__GAUZE__ABSTRACT,
+				description: "updated_at",
+				pre_serialize_middlewares: [$abstract.gauze.middlewares.UPDATED_AT_PROTECTED__MIDDLEWARE__GAUZE__ABSTRACT("gauze__ezuag__updated_at")],
+				serializers: [$abstract.gauze.serializers.UPDATED_AT__SERIALIZER__GAUZE__ABSTRACT("gauze__ezuag__updated_at")],
+				post_serialize_middlewares: [],
+				pre_deserialize_middlewares: [],
+				deserializers: [$abstract.gauze.deserializers.UPDATED_AT__DESERIALIZER__GAUZE__ABSTRACT("gauze__ezuag__updated_at")],
+				post_deserialize_middlewares: [],
+			},
+			gauze__ezuag__deleted_at: {
+				name: "gauze__ezuag__deleted_at",
+				sql_type: "datetime",
+				graphql_type: $abstract.gauze.types.graphql.DATE__GRAPHQL__TYPE__GAUZE__ABSTRACT,
+				description: "deleted_at",
+				pre_serialize_middlewares: [$abstract.gauze.middlewares.DELETED_AT_PROTECTED__MIDDLEWARE__GAUZE__ABSTRACT("gauze__ezuag__deleted_at")],
+				serializers: [$abstract.gauze.serializers.DELETED_AT__SERIALIZER__GAUZE__ABSTRACT("gauze__ezuag__deleted_at")],
+				post_serialize_middlewares: [],
+				pre_deserialize_middlewares: [],
+				deserializers: [$abstract.gauze.deserializers.DELETED_AT__DESERIALIZER__GAUZE__ABSTRACT("gauze__ezuag__deleted_at")],
+				post_deserialize_middlewares: [],
+			},
+			gauze__ezuag__text1: {
+				name: "gauze__ezuag__text1",
+				sql_type: "string",
+				graphql_type: $abstract.gauze.types.graphql.STRING__GRAPHQL__TYPE__GAUZE__ABSTRACT,
+				description: "text1",
+				pre_serialize_middlewares: [],
+				serializers: [],
+				post_serialize_middlewares: [],
+				pre_deserialize_middlewares: [],
+				deserializers: [],
+				post_deserialize_middlewares: [],
+			},
+			gauze__ezuag__text2: {
+				name: "gauze__ezuag__text2",
+				sql_type: "string",
+				graphql_type: $abstract.gauze.types.graphql.STRING__GRAPHQL__TYPE__GAUZE__ABSTRACT,
+				description: "text2",
+				pre_serialize_middlewares: [],
+				serializers: [],
+				post_serialize_middlewares: [],
+				pre_deserialize_middlewares: [],
+				deserializers: [],
+				post_deserialize_middlewares: [],
+			},
+		},
+		methods: {
+			create: {
+				name: "create",
+				privacy: "public",
+			},
+			read: {
+				name: "read",
+				privacy: "private",
+			},
+			update: {
+				name: "update",
+				privacy: "private",
+			},
+			delete: {
+				name: "delete",
+				privacy: "private",
+			},
+		},
+	};
+	ENTITY.graphql_fields = $abstract.helpers.create_graphql_fields(ENTITY);
+	ENTITY.graphql_attributes_string = $abstract.helpers.create_graphql_attributes_string(ENTITY);
+	return ENTITY;
+}
