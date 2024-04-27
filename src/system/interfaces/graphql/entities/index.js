73b9a9b96d0f6c9546a8d5d0c820c7ac0956e58b
Alex Ahn <accounts+technology+github@work.alexahn.com>
2024-04-26 19:53:32 -0700

    format

--- src/system/interfaces/graphql/entities/index.js
+++ src/system/interfaces/graphql/entities/index.js
@@ -1,5 +1,5 @@
-export * as relationship from "./relationship.js"
-export * as whitelist from "./whitelist.js"
+export * as relationship from "./relationship.js";
+export * as whitelist from "./whitelist.js";
 
-export * as entity1 from "./entity1.js"
-export * as entity2 from "./entity2.js"
+export * as entity1 from "./entity1.js";
+export * as entity2 from "./entity2.js";
--- src/system/interfaces/graphql/index.js
+++ src/system/interfaces/graphql/index.js
@@ -1,4 +1,4 @@
 export * as mutations from "./mutations/index.js";
-export * as queries from "./queries/index.js"
-export * as entities from "./entities/index.js"
+export * as queries from "./queries/index.js";
+export * as entities from "./entities/index.js";
 export * as schema from "./schema.js";
