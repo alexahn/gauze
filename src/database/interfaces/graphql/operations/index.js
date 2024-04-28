90e6b9a5668a3ee7cbc0baa29ead3d5ef392d150
Alex Ahn <accounts+technology+github@work.alexahn.com>
2024-04-27 17:23:04 -0700

    blacklist working with stages

--- src/database/interfaces/graphql/operations/index.js
+++ src/database/interfaces/graphql/operations/index.js
@@ -4,6 +4,6 @@ export * as ezuag from "./ezuag/index.js";
 export * as ytitne from "./ytitne/index.js";
 export * as relationship from "./relationship/index.js";
 export * as whitelist from "./whitelist/index.js";
+export * as blacklist from "./blacklist/index.js";
 export * as entity1 from "./entity1/index.js";
 export * as entity2 from "./entity2/index.js";
-export * as blacklist from "./blacklist/index.js";
--- src/structure/blacklist/system/graphql.js
+++ src/structure/blacklist/system/graphql.js
@@ -21,28 +21,7 @@ const METADATA__GRAPHQL__SYSTEM__BLACKLIST__STRUCTURE = new $abstract.gauze.type
 	fields: () => METADATA_FIELDS__GRAPHQL__SYSTEM__BLACKLIST__STRUCTURE,
 });
 
-const ATTRIBUTES_FIELDS__GRAPHQL__SYSTEM__BLACKLIST__STRUCTURE = {
-	id: {
-		type: $abstract.gauze.types.graphql.STRING__GRAPHQL__TYPE__GAUZE__ABSTRACT,
-		description: "id",
-	},
-	created_at: {
-		type: $abstract.gauze.types.graphql.DATE__GRAPHQL__TYPE__GAUZE__ABSTRACT,
-		description: "created_at",
-	},
-	updated_at: {
-		type: $abstract.gauze.types.graphql.DATE__GRAPHQL__TYPE__GAUZE__ABSTRACT,
-		description: "updated_at",
-	},
-	deleted_at: {
-		type: $abstract.gauze.types.graphql.DATE__GRAPHQL__TYPE__GAUZE__ABSTRACT,
-		description: "updated_at",
-	},
-	text: {
-		type: $abstract.gauze.types.graphql.STRING__GRAPHQL__TYPE__GAUZE__ABSTRACT,
-		description: "text",
-	},
-};
+const ATTRIBUTES_FIELDS__GRAPHQL__SYSTEM__BLACKLIST__STRUCTURE = $abstract.entities.blacklist.default($abstract).graphql_fields;
 
 const ATTRIBUTES_FIELDS_ARRAY__GRAPHQL__SYSTEM__BLACKLIST__STRUCTURE = create_fields_array(ATTRIBUTES_FIELDS__GRAPHQL__SYSTEM__BLACKLIST__STRUCTURE);
 
@@ -50,20 +29,6 @@ const ATTRIBUTES__GRAPHQL__SYSTEM__BLACKLIST__STRUCTURE = new $abstract.gauze.ty
 	name: "Blacklist__Attributes",
 	description: "Blacklist Attributes",
 	fields: () => ATTRIBUTES_FIELDS__GRAPHQL__SYSTEM__BLACKLIST__STRUCTURE,
-});
-
-const QUERY_RELATIONSHIPS_FIELDS__GRAPHQL__SYSTEM__BLACKLIST__STRUCTURE = {
-	_metadata: {
-		type: METADATA__GRAPHQL__SYSTEM__BLACKLIST__STRUCTURE,
-		description: "Blacklist Query Metadata",
-	},
-	//read_entity2: ENTITY2_READ_QUERY
-};
-
-const QUERY_RELATIONSHIPS__GRAPHQL__SYSTEM__BLACKLIST__STRUCTURE = new $abstract.gauze.types.graphql.OBJECT__GRAPHQL__TYPE__GAUZE__ABSTRACT({
-	name: "Blacklist_Query__Relationships",
-	description: "Blacklist Query Relationships",
-	fields: () => QUERY_RELATIONSHIPS_FIELDS__GRAPHQL__SYSTEM__BLACKLIST__STRUCTURE,
 });
 
 const QUERY_QUERY_FIELDS__GRAPHQL__SYSTEM__BLACKLIST__STRUCTURE = {};
@@ -74,20 +39,6 @@ const QUERY_QUERY__GRAPHQL__SYSTEM__BLACKLIST__STRUCTURE = new $abstract.gauze.t
 	fields: () => QUERY_QUERY_FIELDS__GRAPHQL__SYSTEM__BLACKLIST__STRUCTURE,
 });
 
-const MUTATION_RELATIONSHIPS_FIELDS__GRAPHQL__SYSTEM__BLACKLIST__STRUCTURE = {
-	_metadata: {
-		type: METADATA__GRAPHQL__SYSTEM__BLACKLIST__STRUCTURE,
-		description: "Blacklist Mutation Metadata",
-	},
-	//create_entity2: ENTITY2_CREATE_MUTATION
-};
-
-const MUTATION_RELATIONSHIPS__GRAPHQL__SYSTEM__BLACKLIST__STRUCTURE = new $abstract.gauze.types.graphql.OBJECT__GRAPHQL__TYPE__GAUZE__ABSTRACT({
-	name: "Blacklist_Mutation__Relationships",
-	description: "Blacklist Mutation Relationships",
-	fields: () => MUTATION_RELATIONSHIPS_FIELDS__GRAPHQL__SYSTEM__BLACKLIST__STRUCTURE,
-});
-
 const MUTATION_MUTATION_FIELDS__GRAPHQL__SYSTEM__BLACKLIST__STRUCTURE = {};
 
 const MUTATION_MUTATION__GRAPHQL__SYSTEM__BLACKLIST__STRUCTURE = new $abstract.gauze.types.graphql.OBJECT__GRAPHQL__TYPE__GAUZE__ABSTRACT({
@@ -96,26 +47,17 @@ const MUTATION_MUTATION__GRAPHQL__SYSTEM__BLACKLIST__STRUCTURE = new $abstract.g
 	fields: () => MUTATION_MUTATION_FIELDS__GRAPHQL__SYSTEM__BLACKLIST__STRUCTURE,
 });
 
-const FIELDS__GRAPHQL__SYSTEM__BLACKLIST__STRUCTURE = {
-	_metadata: {
-		type: METADATA__GRAPHQL__SYSTEM__BLACKLIST__STRUCTURE,
-		description: "_metadata",
-	},
-	attributes: {
-		type: ATTRIBUTES__GRAPHQL__SYSTEM__BLACKLIST__STRUCTURE,
-		description: "attributes",
-	},
-};
-
 const QUERY__GRAPHQL__SYSTEM__BLACKLIST__STRUCTURE = new $abstract.gauze.types.graphql.OBJECT__GRAPHQL__TYPE__GAUZE__ABSTRACT({
 	name: "Blacklist_Query",
-	description: "Entity 1",
+	description: "Blacklist",
 	fields: () => ({
-		_metadata: FIELDS__GRAPHQL__SYSTEM__BLACKLIST__STRUCTURE._metadata,
-		attributes: FIELDS__GRAPHQL__SYSTEM__BLACKLIST__STRUCTURE.attributes,
-		relationships: {
-			type: QUERY_RELATIONSHIPS__GRAPHQL__SYSTEM__BLACKLIST__STRUCTURE,
-			description: "relationships",
+		metadata: {
+			type: METADATA__GRAPHQL__SYSTEM__BLACKLIST__STRUCTURE,
+			description: "metadata",
+		},
+		attributes: {
+			type: ATTRIBUTES__GRAPHQL__SYSTEM__BLACKLIST__STRUCTURE,
+			description: "attributes",
 		},
 		query: {
 			type: QUERY_QUERY__GRAPHQL__SYSTEM__BLACKLIST__STRUCTURE,
@@ -126,13 +68,15 @@ const QUERY__GRAPHQL__SYSTEM__BLACKLIST__STRUCTURE = new $abstract.gauze.types.g
 
 const MUTATION__GRAPHQL__SYSTEM__BLACKLIST__STRUCTURE = new $abstract.gauze.types.graphql.OBJECT__GRAPHQL__TYPE__GAUZE__ABSTRACT({
 	name: "Blacklist_Mutation",
-	description: "Entity 1",
+	description: "Blacklist",
 	fields: () => ({
-		_metadata: FIELDS__GRAPHQL__SYSTEM__BLACKLIST__STRUCTURE._metadata,
-		attributes: FIELDS__GRAPHQL__SYSTEM__BLACKLIST__STRUCTURE.attributes,
-		relationships: {
-			type: MUTATION_RELATIONSHIPS__GRAPHQL__SYSTEM__BLACKLIST__STRUCTURE,
-			description: "relationships",
+		metadata: {
+			type: METADATA__GRAPHQL__SYSTEM__BLACKLIST__STRUCTURE,
+			description: "metadata",
+		},
+		attributes: {
+			type: ATTRIBUTES__GRAPHQL__SYSTEM__BLACKLIST__STRUCTURE,
+			description: "attributes",
 		},
 		mutation: {
 			type: MUTATION_MUTATION__GRAPHQL__SYSTEM__BLACKLIST__STRUCTURE,
@@ -150,10 +94,6 @@ export {
 	ATTRIBUTES__GRAPHQL__SYSTEM__BLACKLIST__STRUCTURE,
 	ATTRIBUTES_FIELDS__GRAPHQL__SYSTEM__BLACKLIST__STRUCTURE,
 	ATTRIBUTES_FIELDS_ARRAY__GRAPHQL__SYSTEM__BLACKLIST__STRUCTURE,
-	QUERY_RELATIONSHIPS__GRAPHQL__SYSTEM__BLACKLIST__STRUCTURE,
-	QUERY_RELATIONSHIPS_FIELDS__GRAPHQL__SYSTEM__BLACKLIST__STRUCTURE,
-	MUTATION_RELATIONSHIPS__GRAPHQL__SYSTEM__BLACKLIST__STRUCTURE,
-	MUTATION_RELATIONSHIPS_FIELDS__GRAPHQL__SYSTEM__BLACKLIST__STRUCTURE,
 	QUERY_QUERY__GRAPHQL__SYSTEM__BLACKLIST__STRUCTURE,
 	QUERY_QUERY_FIELDS__GRAPHQL__SYSTEM__BLACKLIST__STRUCTURE,
 	MUTATION_MUTATION__GRAPHQL__SYSTEM__BLACKLIST__STRUCTURE,
