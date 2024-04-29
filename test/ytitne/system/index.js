f5334e59f14c1e7535d53f994b334d96bd117e80
Alex Ahn <accounts+technology+github@work.alexahn.com>
2024-04-28 21:46:36 -0700

    add relationship tests for ytitne

--- test/ytitne/system/index.js
+++ test/ytitne/system/index.js
@@ -1,2 +1,3 @@
 export * as methods from "./methods/index.js";
 export * as nested from "./nested/index.js";
+export * as relationships from "./relationships/index.js";
---
+++ test/ytitne/system/relationships/delete/2.read.js
@@ -0,0 +1,46 @@
+const description = `Read a ytitne object`;
+const operation_name = "ReadYtitne";
+const operation_source = `
+query ${operation_name}($where: Ytitne_Query__Attributes, $where2: Ytitne_Query__Attributes) {
+	read_ytitne(where: $where) {
+		attributes {
+			id
+			text
+		}
+	}
+	read_ytitne2: read_ytitne(where: $where2) {
+		attributes {
+			id
+			text
+		}
+	}
+}
+`;
+const operation_variables = {
+	where: {
+		id: "3ab515f4-5391-4af9-a2c9-a7119ad262ce",
+	},
+	where2: {
+		id: "55c29b50-5e24-447d-8505-75e82c2aa8cc",
+	},
+};
+const expected = `{
+    "data": {
+        "read_ytitne": [],
+        "read_ytitne2": []
+    }
+}`;
+
+export default {
+	step: 2,
+	description: description,
+	context: {
+		agent_id: "1",
+	},
+	operation: {
+		name: operation_name,
+		source: operation_source,
+		variables: operation_variables,
+	},
+	expected: expected,
+};
