ce8eaacb97b6d2527b40692f13a081703b81d1f9
Alex Ahn <accounts+technology+github@work.alexahn.com>
2024-04-28 20:43:40 -0700

    add agent_id to query and mutation interfaces

--- src/system/interfaces/graphql/mutations/ezuag.js
+++ src/system/interfaces/graphql/mutations/ezuag.js
@@ -54,6 +54,7 @@ const CREATE__EZUAG__MUTATION__GRAPHQL__INTERFACE__SYSTEM = {
 				source: _source,
 				database: context.database,
 				transaction: context.transaction,
+				agent_id: context.agent_id,
 			},
 			mutation_arguments,
 		).then(function (data) {
