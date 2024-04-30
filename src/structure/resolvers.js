98ab795f96e914e95e0abe06c02ecd45e889e94e
Alex Ahn <accounts+technology+github@work.alexahn.com>
2024-04-29 19:50:21 -0700

    refactor structure/resolvers to dynamically construct graphql meta type to sql table mapping

--- src/structure/resolvers.js
+++ src/structure/resolvers.js
@@ -1,24 +1,4 @@
-// ezuag
-import { TYPE__GRAPHQL__DATABASE__EZUAG__STRUCTURE } from "./ezuag/database/graphql.js";
-import { TABLE_NAME__SQL__DATABASE__EZUAG__STRUCTURE } from "./ezuag/database/sql.js";
-// ytitne
-import { TYPE__GRAPHQL__DATABASE__YTITNE__STRUCTURE } from "./ytitne/database/graphql.js";
-import { TABLE_NAME__SQL__DATABASE__YTITNE__STRUCTURE } from "./ytitne/database/sql.js";
-// relationship
-import { TYPE__GRAPHQL__DATABASE__RELATIONSHIP__STRUCTURE } from "./relationship/database/graphql.js";
-import { TABLE_NAME__SQL__DATABASE__RELATIONSHIP__STRUCTURE } from "./relationship/database/sql.js";
-// whitelist
-import { TYPE__GRAPHQL__DATABASE__WHITELIST__STRUCTURE } from "./whitelist/database/graphql.js";
-import { TABLE_NAME__SQL__DATABASE__WHITELIST__STRUCTURE } from "./whitelist/database/sql.js";
-// blacklist
-import { TYPE__GRAPHQL__DATABASE__BLACKLIST__STRUCTURE } from "./blacklist/database/graphql.js";
-import { TABLE_NAME__SQL__DATABASE__BLACKLIST__STRUCTURE } from "./blacklist/database/sql.js";
-// entity1
-import { TYPE__GRAPHQL__DATABASE__ENTITY1__STRUCTURE } from "./entity1/database/graphql.js";
-import { TABLE_NAME__SQL__DATABASE__ENTITY1__STRUCTURE } from "./entity1/database/sql.js";
-// entity2
-import { TYPE__GRAPHQL__DATABASE__ENTITY2__STRUCTURE } from "./entity2/database/graphql.js";
-import { TABLE_NAME__SQL__DATABASE__ENTITY2__STRUCTURE } from "./entity2/database/sql.js";
+import * as $abstract from "./../abstract/index.js";
 
 function invert(obj) {
 	const inverted = {};
@@ -29,18 +9,20 @@ function invert(obj) {
 	return inverted;
 }
 
-const DATABASE_GRAPHQL_TYPE_TO_SQL_TABLE = {
-	[TYPE__GRAPHQL__DATABASE__EZUAG__STRUCTURE]: TABLE_NAME__SQL__DATABASE__EZUAG__STRUCTURE,
-	[TYPE__GRAPHQL__DATABASE__YTITNE__STRUCTURE]: TABLE_NAME__SQL__DATABASE__YTITNE__STRUCTURE,
-	[TYPE__GRAPHQL__DATABASE__RELATIONSHIP__STRUCTURE]: TABLE_NAME__SQL__DATABASE__RELATIONSHIP__STRUCTURE,
-	[TYPE__GRAPHQL__DATABASE__WHITELIST__STRUCTURE]: TABLE_NAME__SQL__DATABASE__WHITELIST__STRUCTURE,
-	[TYPE__GRAPHQL__DATABASE__BLACKLIST__STRUCTURE]: TABLE_NAME__SQL__DATABASE__BLACKLIST__STRUCTURE,
-	[TYPE__GRAPHQL__DATABASE__ENTITY1__STRUCTURE]: TABLE_NAME__SQL__DATABASE__ENTITY1__STRUCTURE,
-	[TYPE__GRAPHQL__DATABASE__ENTITY2__STRUCTURE]: TABLE_NAME__SQL__DATABASE__ENTITY2__STRUCTURE,
-};
-const DATABASE_SQL_TABLE_TO_GRAPHQL_TYPE = invert(DATABASE_GRAPHQL_TYPE_TO_SQL_TABLE);
+function construct_mapping_from_modules(modules) {
+	const mapping = {};
+	const entities = Object.keys(modules)
+		.map(function (key) {
+			const module = modules[key];
+			return module.default($abstract);
+		})
+		.forEach(function (entity) {
+			mapping[entity.graphql_meta_type] = entity.table_name;
+		});
+	return mapping;
+}
 
-const SYSTEM_GRAPHQL_TYPE_TO_SQL_TABLE = DATABASE_GRAPHQL_TYPE_TO_SQL_TABLE;
-const SYSTEM_SQL_TABLE_TO_GRAPHQL_TYPE = DATABASE_SQL_TABLE_TO_GRAPHQL_TYPE;
+const GRAPHQL_TYPE_TO_SQL_TABLE__RESOLVER__STRUCTURE = construct_mapping_from_modules($abstract.entities);
+const SQL_TABLE_TO_GRAPHQL_TYPE__RESOLVER__STRUCTURE = invert(GRAPHQL_TYPE_TO_SQL_TABLE__RESOLVER__STRUCTURE);
 
-export { DATABASE_GRAPHQL_TYPE_TO_SQL_TABLE, DATABASE_SQL_TABLE_TO_GRAPHQL_TYPE, SYSTEM_GRAPHQL_TYPE_TO_SQL_TABLE, SYSTEM_SQL_TABLE_TO_GRAPHQL_TYPE };
+export { GRAPHQL_TYPE_TO_SQL_TABLE__RESOLVER__STRUCTURE, SQL_TABLE_TO_GRAPHQL_TYPE__RESOLVER__STRUCTURE };
