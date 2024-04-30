b482ae3b36378a165dc00751b6c88f58bf9689cf
Alex Ahn <accounts+technology+github@work.alexahn.com>
2024-04-29 19:14:47 -0700

    recompile

--- src/kernel/src/base/structure/gauze/database/sql.js
+++ src/kernel/src/base/structure/gauze/database/sql.js
@@ -1,6 +1,6 @@
 import * as $abstract from "./../../../abstract/index.js";
 
-const TABLE_NAME__SQL__DATABASE__caf5342ac38d41a6a02bb81d2d2b21a4__STRUCTURE = "gauze__a543731262804f64adcc0eae1a225acc";
+const TABLE_NAME__SQL__DATABASE__caf5342ac38d41a6a02bb81d2d2b21a4__STRUCTURE = $abstract.entities.a543731262804f64adcc0eae1a225acc.default($abstract).table_name;
 const PRIMARY_KEY__SQL__DATABASE__caf5342ac38d41a6a02bb81d2d2b21a4__STRUCTURE = $abstract.entities.a543731262804f64adcc0eae1a225acc.default($abstract).primary_key;
 
 export { TABLE_NAME__SQL__DATABASE__caf5342ac38d41a6a02bb81d2d2b21a4__STRUCTURE, PRIMARY_KEY__SQL__DATABASE__caf5342ac38d41a6a02bb81d2d2b21a4__STRUCTURE };
--- src/structure/entity2/database/sql.js
+++ src/structure/entity2/database/sql.js
@@ -1,6 +1,6 @@
 import * as $abstract from "./../../../abstract/index.js";
 
-const TABLE_NAME__SQL__DATABASE__ENTITY2__STRUCTURE = "gauze__entity2";
+const TABLE_NAME__SQL__DATABASE__ENTITY2__STRUCTURE = $abstract.entities.entity2.default($abstract).table_name;
 const PRIMARY_KEY__SQL__DATABASE__ENTITY2__STRUCTURE = $abstract.entities.entity2.default($abstract).primary_key;
 
 export { TABLE_NAME__SQL__DATABASE__ENTITY2__STRUCTURE, PRIMARY_KEY__SQL__DATABASE__ENTITY2__STRUCTURE };
