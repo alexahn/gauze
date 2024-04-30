5377a295c89607a1a0b921cf48d0dd6ee3388610
Alex Ahn <accounts+technology+github@work.alexahn.com>
2024-04-29 19:59:21 -0700

    add new resolvers

--- src/structure/resolvers.js
+++ src/structure/resolvers.js
@@ -9,7 +9,7 @@ function invert(obj) {
 	return inverted;
 }
 
-function construct_mapping_from_modules(modules) {
+function construct_mapping_from_modules(modules, from_key, to_key) {
 	const mapping = {};
 	const entities = Object.keys(modules)
 		.map(function (key) {
@@ -17,12 +17,19 @@ function construct_mapping_from_modules(modules) {
 			return module.default($abstract);
 		})
 		.forEach(function (entity) {
-			mapping[entity.graphql_meta_type] = entity.table_name;
+			mapping[entity[from_key]] = entity[to_key];
 		});
 	return mapping;
 }
 
-const GRAPHQL_TYPE_TO_SQL_TABLE__RESOLVER__STRUCTURE = construct_mapping_from_modules($abstract.entities);
+const GRAPHQL_TYPE_TO_SQL_TABLE__RESOLVER__STRUCTURE = construct_mapping_from_modules($abstract.entities, "graphql_meta_type", "table_name");
 const SQL_TABLE_TO_GRAPHQL_TYPE__RESOLVER__STRUCTURE = invert(GRAPHQL_TYPE_TO_SQL_TABLE__RESOLVER__STRUCTURE);
+const MODULE_NAME_TO_SQL_TABLE__RESOLVER__STRUCTURE = construct_mapping_from_modules($abstract.entities, "name", "table_name");
+const SQL_TABLE_TO_MODULE_NAME__RESOLVER__STRUCTURE = invert(MODULE_NAME_TO_SQL_TABLE__RESOLVER__STRUCTURE);
 
-export { GRAPHQL_TYPE_TO_SQL_TABLE__RESOLVER__STRUCTURE, SQL_TABLE_TO_GRAPHQL_TYPE__RESOLVER__STRUCTURE };
+export {
+	GRAPHQL_TYPE_TO_SQL_TABLE__RESOLVER__STRUCTURE,
+	SQL_TABLE_TO_GRAPHQL_TYPE__RESOLVER__STRUCTURE,
+	MODULE_NAME_TO_SQL_TABLE__RESOLVER__STRUCTURE,
+	SQL_TABLE_TO_MODULE_NAME__RESOLVER__STRUCTURE,
+};
