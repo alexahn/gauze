5e5a2a3440a16c33a388a90b696d42f92af8002b
Alex Ahn <accounts+technology+github@work.alexahn.com>
2024-04-30 14:31:32 -0700

    delete entity1 and entity2 tests

--- test/entity1/index.js
+++
@@ -1,1 +0,0 @@
-export * as database_interface_graphql from "./database_interface_graphql.js";
--- test/index.js
+++ test/index.js
@@ -1,5 +1,5 @@
 export * as relationship from "./relationship/index.js";
-export * as entity1 from "./entity1/index.js";
-export * as entity2 from "./entity2/index.js";
+//export * as entity1 from "./entity1/index.js";
+//export * as entity2 from "./entity2/index.js";
 
 export * as ytitne from "./ytitne/index.js";
