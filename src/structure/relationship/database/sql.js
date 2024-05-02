7e0f47ba8565d02126339291cf36d021b8474ff8
Alex Ahn <accounts+technology+github@work.alexahn.com>
2024-05-02 01:16:52 -0700

    remove dead code

--- src/structure/relationship/database/sql.js
+++
@@ -1,6 +0,0 @@
-import * as $abstract from "./../../../abstract/index.js";
-
-const TABLE_NAME__SQL__DATABASE__RELATIONSHIP__STRUCTURE = $abstract.entities.relationship.default($abstract).table_name;
-const PRIMARY_KEY__SQL__DATABASE__RELATIONSHIP__STRUCTURE = $abstract.entities.relationship.default($abstract).primary_key;
-
-export { TABLE_NAME__SQL__DATABASE__RELATIONSHIP__STRUCTURE, PRIMARY_KEY__SQL__DATABASE__RELATIONSHIP__STRUCTURE };
--- src/structure/whitelist/database/sql.js
+++
@@ -1,6 +0,0 @@
-import * as $abstract from "./../../../abstract/index.js";
-
-const TABLE_NAME__SQL__DATABASE__WHITELIST__STRUCTURE = $abstract.entities.whitelist.default($abstract).table_name;
-const PRIMARY_KEY__SQL__DATABASE__WHITELIST__STRUCTURE = $abstract.entities.whitelist.default($abstract).primary_key;
-
-export { TABLE_NAME__SQL__DATABASE__WHITELIST__STRUCTURE, PRIMARY_KEY__SQL__DATABASE__WHITELIST__STRUCTURE };
