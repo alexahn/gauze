f49cb2a23fdadb97c4e67ba12b17cec7670e606e
Alex Ahn <accounts+technology+github@work.alexahn.com>
2024-05-02 01:45:19 -0700

    minor

--- src/structure/index.js
+++ src/structure/index.js
@@ -1,5 +1,5 @@
 export * as gauze from "./gauze/index.js";
-export * as project from "./project/index.js"
+export * as project from "./project/index.js";
 export * as entities from "./entities/index.js";
 
 export * as relationships from "./relationships.js";
