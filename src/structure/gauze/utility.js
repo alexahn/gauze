9017607365758ebc03e56f6d10c80263a0ef7ac0
Alex Ahn <accounts+technology+github@work.alexahn.com>
2024-05-02 01:32:53 -0700

    migrating structure/utility.js to structure/gauze/utility.js

---
+++ src/structure/gauze/utility.js
@@ -0,0 +1,16 @@
+import * as $abstract from "./../abstract/index.js";
+
+function create_fields_array(fields) {
+	const keys = Object.keys(fields);
+	const mapped = {};
+	keys.forEach(function (key) {
+		const value = fields[key];
+		mapped[key] = {
+			type: new $abstract.gauze.types.graphql.LIST__GRAPHQL__TYPE__GAUZE__ABSTRACT(value.type),
+			description: value.description,
+		};
+	});
+	return mapped;
+}
+
+export { create_fields_array };
