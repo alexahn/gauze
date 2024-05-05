52f2e9827407f9d6543a71b9178c2e9eab84338e
Alex Ahn <accounts+technology+github@work.alexahn.com>
2024-05-04 20:27:22 -0700

    rename src/structure/relationships but avoid naming conflicts with relationship entity

--- src/kernel/src/stages/stage4/database/interfaces/graphql/schema.js
+++ src/kernel/src/stages/stage4/database/interfaces/graphql/schema.js
@@ -3,7 +3,7 @@ import { GraphQLSchema } from "graphql";
 import * as $abstract from "./../../../abstract/index.js";
 import * as $structure from "./../../../structure/index.js";
 
-import { DATABASE_RELATIONSHIP_STRUCTURE } from "./../../../structure/relationships.js";
+import { DATABASE_RELATIONSHIPS__RELATIONSHIP__STRUCTURE } from "./../../../structure/relationships.js";
 
 // ezuag
 import { CONNECTION__EZUAG__ENTITY__GRAPHQL__INTERFACE__DATABASE, METHODS__EZUAG__ENTITY__GRAPHQL__INTERFACE__DATABASE } from "./entities/ezuag.js";
@@ -33,8 +33,8 @@ const METHODS = {
 
 // todo: add some existence checks and guards to these loops
 function link() {
-	Object.keys(DATABASE_RELATIONSHIP_STRUCTURE).forEach(function (entity) {
-		var relationships = DATABASE_RELATIONSHIP_STRUCTURE[entity];
+	Object.keys(DATABASE_RELATIONSHIPS__RELATIONSHIP__STRUCTURE).forEach(function (entity) {
+		var relationships = DATABASE_RELATIONSHIPS__RELATIONSHIP__STRUCTURE[entity];
 		relationships.forEach(function (related) {
 			Object.keys(METHODS[related].query).forEach(function (query) {
 				var query_method = METHODS[related].query[query];
