f04a863a9cd64eb24a5c000e2c2d67f3c67a027a
Alex Ahn <accounts+technology+github@work.alexahn.com>
2024-05-05 04:47:02 -0700

    minor

--- src/kernel/ratelimit.js
+++ src/kernel/ratelimit.js
@@ -1,10 +1,3 @@
-function limit (x, t0, t1, r) {
-	console.log('distance', t1 - t0)
-	console.log('partial',  (1 / ((t1 - t0) * r)))
-	console.log('degen', ((t1 - t0) * r))
-	return Math.max(0, x - ((t1 - t0) * r)) //+ (1 / ((t1 - t0) * r))
-}
-
 const start = new Date().getTime()
 
 var passed = 0
@@ -20,24 +13,24 @@ function test () {
 	setInterval(function () {
 		counter += 1
 		var t1 = new Date().getTime()
-		//var x1 = degen(x0, t0, t1, rm)
-		//var scaled = (1 / ((t1 - t0) * rm))
 		if (x0 < rs) {
-			//x0 = regen(x1, t0, t1, rm)
 			x0 = regen(x0, t0, t1, rm)
 			t0 = t1
 			passed += 1
 			//console.log('passed', passed)
 		} else {
 			x0 = degen(x0, t0, t1, rm)
-			//t0 = t1
-			//x0 = x1
 			t0 = t1
+			// attempt
+			/*
+			if (x0 < rs) {
+				x0 = regen(x0, t0, t1, rm)
+				passed += 1
+			} else {
+				rejected += 1
+			}
+			*/
 			rejected += 1
-			//console.log('rejected', rejected)
-			//const end = new Date().getTime()
-			//console.log('duration', end - start)
-			
 		}
 		if (counter % 100 === 0) {
 			console.log('passed', counter / 100, passed)
@@ -49,14 +42,9 @@ function test () {
 test()
 
 function degen (x, t0, t1, r) {
-	//console.log('degen', ((t1 - t0) * r))
 	return Math.max(0, x - ((t1 - t0) * r))
 }
 
 function regen (x, t0, t1, r) {
-	//console.log('regen', (1 / ((t1 - t0) * r)))
-	//return x + (1 / (r * 1000))
 	return x + 1
-	//return x + (1 / ((t1 - t0) * r))
-	//return Math.max(0, x + ((t1 - t0) / r))
 }
