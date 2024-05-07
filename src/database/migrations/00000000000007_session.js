ead41106082ee952dd3f918df093366f3bdbf0f0
Alex Ahn <accounts+technology+github@work.alexahn.com>
2024-05-06 23:43:24 -0700

    copy migrations from kernel to database

---
+++ src/database/migrations/00000000000007_session.js
@@ -0,0 +1,39 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+const up = function (knex) {
+	return Promise.all([
+		knex.schema.createTable("gauze__session", function (table) {
+			table.uuid("gauze__session__id", { useBinaryUuid: true, primaryKey: true }).primary().defaultTo(knex.fn.uuid());
+			table.date("gauze__session__created_at").notNullable();
+			table.date("gauze__session__updated_at").notNullable();
+			table.date("gauze__session__deleted_at");
+
+			table.string("gauze__session__realm", 16).notNullable();
+			table.string("gauze__session__agent_type", 64);
+			table.uuid("gauze__session__agent_id");
+			table.string("gauze__session__value", 512).notNullable();
+			table.string("gauze__session__kind", 16).notNullable();
+			table.string("gauze__session__data", 512).notNullable();
+
+			table.index("gauze__session__realm");
+			table.index("gauze__session__agent_type");
+			table.index("gauze__session__agent_id");
+
+			table.unique(["gauze__session__realm", "gauze__session__agent_type", "gauze__session__agent_id", "gauze__session__value"], {
+				useConstraint: true,
+			});
+		}),
+	]);
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+const down = function (knex) {
+	return Promise.all([knex.schema.dropTable("gauze__session")]);
+};
+
+export { up, down };
---
+++ src/database/migrations/00000000000013_agent_character.js
@@ -0,0 +1,24 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+const up = function (knex) {
+	return Promise.all([
+		knex.schema.createTable("gauze__agent_character", function (table) {
+			table.uuid("id", { useBinaryUuid: true, primaryKey: true }).primary().defaultTo(knex.fn.uuid());
+			table.date("created_at").notNullable();
+			table.date("updated_at").notNullable();
+			table.date("deleted_at");
+		}),
+	]);
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+const down = function (knex) {
+	return Promise.all([knex.schema.dropTable("gauze__agent_character")]);
+};
+
+export { up, down };
