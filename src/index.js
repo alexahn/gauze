faf13060c9c3e0b119797c1338954f1826bd0c42
Alex Ahn <accounts+technology+github@work.alexahn.com>
2024-05-07 20:35:24 -0700

    remove environment from kernel stages and modify src from base to unjoin and join at beginning and end

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ export * as structure from "./structure/index.js";
 export * as kernel from "./kernel/index.js";
 export * as database from "./database/index.js";
 export * as system from "./system/index.js";
-export * as environment from "./environment/index.js";
+export * as enviroment from "./enviroment/index.js";
