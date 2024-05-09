96f6f1cd9b5075d99ce48f4c86b34aeb07cba486
Alex Ahn <accounts+technology+github@work.alexahn.com>
2024-05-09 07:18:07 -0700

    prepare to add pascal case to class names

--- src/system/models/ytitne.js
+++ src/system/models/ytitne.js
@@ -3,7 +3,7 @@ import * as $structure from "./../../structure/index.js";
 import * as $database from "./../../database/index.js";
 import * as $kernel from "./../../kernel/index.js";
 
-class YtitneSystemModel extends $kernel.models.system.SystemModel {
+class YtitneYtitneSystemModel extends $kernel.models.system.SystemModel {
 	constructor(root_config, config) {
 		super(root_config, config);
 		const self = this;
