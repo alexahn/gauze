495ffc9f7f3fb2a00c5a1bf28a2ba1ee89bb6998
Alex Ahn <accounts+technology+github@work.alexahn.com>
2024-05-16 21:55:05 -0700

    delete old entity validation

--- src/kernel/applications/manager.js
+++ src/kernel/applications/manager.js
@@ -107,8 +107,8 @@ class GauzeManager {
 	}
 	validate_project_config(config) {}
 	read_project_config(config_path) {}
-	validate_entity_config2(config) {
-		const self = this
+	validate_entity_config(config) {
+		const self = this;
 
 		const valid_entity_keys = {
 			name: true,
@@ -277,7 +277,7 @@ class GauzeManager {
 				} else if (key === "privacy") {
 					const privacy = method[key];
 					if (typeof privacy !== "string") throw new Error(`Entity property '${path}' must be of type 'string', ${privacy} is not of type 'string'`);
-					if (!valid_privacy[privacy]) throw new Error(`Entity property '${path}' must contain string values from (${Object.keys(valid_privacy)}): ${privacy} is not contained`)
+					if (!valid_privacy[privacy]) throw new Error(`Entity property '${path}' must contain string values from (${Object.keys(valid_privacy)}): ${privacy} is not contained`);
 				} else if (key === "allowed_agent_types") {
 					validate_agent_types(path, method[key]);
 				} else {
@@ -337,115 +337,11 @@ class GauzeManager {
 			}
 		});
 	}
-	validate_entity_config(config) {
-		const self = this;
-		function validate_reducers(reducers) {
-			reducers.forEach(function (reducer) {
-				if (typeof reducer !== "object") throw new Error(`Reducer must be of type 'object': ${reducer}`);
-				if (typeof reducer.create !== "function") throw new Error(`Reducer for create method must be of type 'function': ${reducer.create}`);
-				if (typeof reducer.read !== "function") throw new Error(`Reducer for read method must be of type 'function': ${reducer.read}`);
-				if (typeof reducer.update !== "function") throw new Error(`Reducer for update method must be of type 'function': ${reducer.update}`);
-				if (typeof reducer.delete !== "function") throw new Error(`Reducer for delete method must be of type 'function': ${reducer.delete}`);
-			});
-		}
-		// should have a name attribute
-		if (typeof config.name !== "string") throw new Error(`Entity must have a 'name' attribute of type 'string': ${config.name}`);
-		if (to_snake_case(config.name) !== config.name) throw new Error(`Entity must have a 'name' attribute in lower snake case: ${config.name} !== ${to_snake_case(config.name)}`);
-		// should have a table_name attribute
-		if (typeof config.table_name !== "string") throw new Error(`Entity must have a 'table_name' attribute of type 'string': ${config.table_name}`);
-		// should have a primary_key attribute
-		if (typeof config.primary_key !== "string") throw new Error(`Entity must have a 'primary_key' attribute of type 'string': ${config.primary_key}`);
-		// should have a fields attribute
-		if (typeof config.fields !== "object") throw new Error(`Entity must have a 'fields' attribute of type 'object': ${config.fields}`);
-		Object.keys(config.fields).forEach(function (key) {
-			const field = config.fields[key];
-			// name
-			if (typeof field.name !== "string") throw new Error(`Field must have a 'name' attribute of type 'string': ${field.name}`);
-			if (field.name !== key) throw new Error(`Field 'name' attribute must align with the key in the 'fields' attribute: ${field.name} !== ${key}`);
-			// sql_type
-			if (typeof field.sql_type !== "string") throw new Error(`Field must have a 'sql_type' attribute of type 'string': ${field.sql_type}`);
-			// graphql_type
-			if (typeof field.graphql_type !== "object") throw new Error(`Field must have a 'graphql_type' attribute of type 'object': ${field.grapghql_type}`);
-			// description
-			if (typeof field.description !== "string") throw new Error(`Field must have a 'description' attribute of type 'string': ${field.description}`);
-			// pre_serialize_middlewares
-			if (typeof field.pre_serialize_middlewares !== "object")
-				throw new Error(`Field must have a 'pre_serialize_middlewares' attribute of type 'object': ${field.pre_serialize_middlewares}`);
-			if (typeof field.pre_serialize_middlewares.length !== "number")
-				throw new Error(`Field 'pre_serialize_middlewares' attribute must have length attribute of type 'number': ${field.pre_serialize_middlewares.length}`);
-			validate_reducers(field.pre_serialize_middlewares);
-			// serializers
-			if (typeof field.serializers !== "object") throw new Error(`Field must have a 'serializers' attribute of type 'object': ${field.serializers}`);
-			if (typeof field.serializers.length !== "number") throw new Error(`Field 'serializers' attribute must have length attribute of type 'number': ${field.serializers.length}`);
-			validate_reducers(field.serializers);
-			// post_serialize_middlewares
-			if (typeof field.post_serialize_middlewares !== "object")
-				throw new Error(`Field must have a 'post_serialize_middlewares' attribute of type 'object': ${field.post_serialize_middlewares}`);
-			if (typeof field.post_serialize_middlewares.length !== "number")
-				throw new Error(`Field 'post_serialize_middlewares' attribute must have length attribute of type 'number': ${field.post_serialize_middlewares.length}`);
-			validate_reducers(field.post_serialize_middlewares);
-			// pre_deserialize_middlewares
-			if (typeof field.pre_deserialize_middlewares !== "object")
-				throw new Error(`Field must have a 'pre_deserialize_middlewares' attribute of type 'object': ${field.pre_deserialize_middlewares}`);
-			if (typeof field.pre_deserialize_middlewares.length !== "number")
-				throw new Error(`Field 'pre_deserialize_middlewares' attribute must have length attribute of type 'number': ${field.pre_deserialize_middlewares.length}`);
-			validate_reducers(field.pre_deserialize_middlewares);
-			// deserializers
-			if (typeof field.deserializers !== "object") throw new Error(`Field must have a 'deserializers' attribute of type 'object': ${field.deserializers}`);
-			if (typeof field.deserializers.length !== "number") throw new Error(`Field 'deserializers' attribute must have length attribute of type 'number': ${field.deserializers.length}`);
-			validate_reducers(field.deserializers);
-			// post_deserialize_middlewares
-			if (typeof field.post_deserialize_middlewares !== "object")
-				throw new Error(`Field must have a 'post_deserialize_middlewares' attribute of type 'object': ${field.post_deserialize_middlewares}`);
-			if (typeof field.post_deserialize_middlewares.length !== "number")
-				throw new Error(`Field 'post_deserialize_middlewares' attribute must have length attribute of type 'number': ${field.post_deserialize_middlewares.length}`);
-			validate_reducers(field.post_deserialize_middlewares);
-			if (typeof field.allowed_agent_types !== "object") throw new Error(`Field 'allowed_agent_types' attribute must be of type 'object': ${field.allowed_agent_types}`);
-			if (typeof field.allowed_agent_types.length !== "number") throw new Error(`Field 'allowed_agent_types' attribute must be of type 'array': ${field.allowed_agent_types}`);
-			field.allowed_agent_types.forEach(function (agent_type) {
-				if (typeof agent_type !== "string") throw new Error(`Field attribute 'allowed_agent_types' must contain only string values`);
-				if (agent_type === config.table_name) {
-					if (!self.valid_agent_type_exceptions[agent_type])
-						throw new Error(`Field attribute 'allowed_agent_types' must contain string values from (${Object.keys(self.valid_agent_type_exceptions)}): ${agent_type}`);
-				} else {
-					if (!self.valid_agent_types[agent_type])
-						throw new Error(`Field attribute 'allowed_agent_types' must contain string values from (${Object.keys(self.valid_agent_types)}): ${agent_type}`);
-				}
-			});
-		});
-		// methods
-		if (typeof config.methods !== "object") throw new Error(`Entity must have a 'methods' attribute of type 'object': ${config.methods}`);
-		Object.keys(config.methods).forEach(function (key) {
-			const method = config.methods[key];
-			// name
-			if (typeof method.name !== "string") throw new Error(`Method must have a 'name' attribute of type 'string': ${method.name}`);
-			if (method.name !== key) throw new Error(`Method 'name' attribute must align with the key in the 'methods' attribute: ${method.name} !== ${key}`);
-			// privacy
-			if (typeof method.privacy !== "string") throw new Error(`Method must have a 'privacy' attribute of type 'object': ${method.privacy}`);
-			if (typeof method.allowed_agent_types !== "object") throw new Error(`Method must have a 'allowed_agent_types' attribute of type 'array': ${method.allowed_agent_types}`);
-			if (typeof method.allowed_agent_types.length !== "number") throw new Error(`Method must have a 'allowed_agent_types' attribute of type 'array': ${method.allowed_agent_types}`);
-			method.allowed_agent_types.forEach(function (agent_type) {
-				if (typeof agent_type !== "string") throw new Error(`Method attribute 'allowed_agent_types' must contain only string values`);
-				if (agent_type === config.table_name) {
-					if (!self.valid_agent_type_exceptions[agent_type])
-						throw new Error(`Method attribute 'allowed_agent_types' must contain string values from (${Object.keys(self.valid_agent_type_exceptions)}): ${agent_type}`);
-				} else {
-					if (!self.valid_agent_types[agent_type])
-						throw new Error(`Method attribute 'allowed_agent_types' must contain string values from (${Object.keys(self.valid_agent_types)}): ${agent_type}`);
-				}
-			});
-		});
-		// graphql_fields
-		if (typeof config.graphql_fields !== "object") throw new Error(`Entity must have a 'graphql_fields' attribute of type 'object': ${config.graphql_fields}`);
-		// graphql_attributes_string
-		if (typeof config.graphql_attributes_string !== "string")
-			throw new Error(`Entity must have a 'graphql_attributes_string' attribute of type 'string': ${config.graphql_attributes_string}`);
-	}
 	read_entity_config(config_path) {
 		const self = this;
 		return import(config_path).then(function (config) {
 			const ENTITY = config.default(self.$gauze.abstract);
-			self.validate_entity_config2(ENTITY);
+			self.validate_entity_config(ENTITY);
 			return Promise.resolve(ENTITY);
 		});
 	}
