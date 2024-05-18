846a6444c3a4f9a54d491d6cd9481692c3d74c76
Alex Ahn <accounts+technology+github@work.alexahn.com>
2024-05-17 21:45:12 -0700

    minor

--- src/kernel/applications/gauze/src/layouts/Alligator.jsx
+++ src/kernel/applications/gauze/src/layouts/Alligator.jsx
@@ -5,8 +5,8 @@ export default function Alligator({ sections, units }) {
 	const id = "layout:alligator";
 	return (
 		<div id={id} key={id} className="flex justify-center w-100">
-			<div className="flex mw9 cf w-100 flex-column">
-				<div className="fl w-100 pa4">
+			<div className="flex cf mw9 w-100 flex-column">
+				<div className="fl w-100 pa4 pb2">
 					<sections.top units={units.top} />
 				</div>
 				{/*
@@ -14,7 +14,7 @@ export default function Alligator({ sections, units }) {
 					<hr />
 				</div>
 				*/}
-				<div className="fl w-100 pa4">
+				<div className="fl w-100 pl4 pr4">
 					<sections.bottom units={units.bottom} />
 				</div>
 			</div>
