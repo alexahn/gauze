43dd0910a2ef15a2f6e904b9a987d1b7049d2280
Alex Ahn <accounts+technology+github@work.alexahn.com>
2024-05-30 09:37:26 -0700

    fix

--- src/kernel/applications/manager.js
+++ src/kernel/applications/manager.js
@@ -343,7 +343,7 @@ class GauzeManager {
 				if (typeof graphql_meta_type !== "string") throw new Error(`Entity property '${path}' must be of type 'string', ${graphql_meta_type} is not of type 'string'`);
 			} else if (key === "default_order") {
 				const default_order = config[key];
-				const valid_default_orders = Object.keys(config.fields);
+				const valid_default_orders = config.fields;
 				if (typeof default_order !== "string") throw new Error(`Entity property '${path}' must be of type 'string', ${default_order} is not of type 'string'`);
 				if (!valid_default_orders[default_order])
 					throw new Error(`Entity property '${path}' must contain string values from (${Object.keys(valid_default_orders)}): ${default_order} is not contained`);
