8ec6690a526647f4ab1a2e98b59ffca56717947a
Alex Ahn <accounts+technology+github@work.alexahn.com>
2024-06-18 08:06:51 -0700

    use a loading svg animation

--- src/kernel/applications/gauze/src/route.jsx
+++ src/kernel/applications/gauze/src/route.jsx
@@ -1,6 +1,8 @@
 import * as React from "react";
 import { useDispatch, useSelector } from "react-redux";
 
+import blocks from "./static/blocks-wave.svg";
+
 export default function Router() {
 	const view = useSelector((state) => {
 		return state.view;
@@ -8,6 +10,10 @@ export default function Router() {
 	if (view && view.layout && view.sections && view.units) {
 		return <view.layout sections={view.sections} units={view.units} />;
 	} else {
-		return <div>Loading</div>;
+		return (
+			<div className="flex justify-center items-center w-100 h-100 bgx12">
+				<img className="loading" src={blocks} width="33%" height="33%" />
+			</div>
+		);
 	}
 }
