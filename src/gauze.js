d733af2ca58e9b24e26922d5bc9c12e353337674
Alex Ahn <accounts+technology+github@work.alexahn.com>
2025-07-15 04:51:39 -0700

    process and http middlewares placeholder

--- src/gauze.js
+++ src/gauze.js
@@ -2,6 +2,18 @@ export default {
 	name: "gauze",
 	type: "project",
 	version: "0.0.1",
+	realms: [
+		"./kernel",
+		"./database",
+		"./system",
+		"./environment"
+	],
+	process_middlewares: [
+
+	],
+	http_middlewares: [
+
+	],
 	development: {
 		admins: [
 			{
