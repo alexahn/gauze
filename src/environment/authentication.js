d73da687567945e4bc9f65d783051f4050cf3e0f
Alex Ahn <accounts+technology+github@work.alexahn.com>
2025-07-16 15:43:38 -0700

    set up database jwt authentication functions

--- src/environment/authentication.js
+++ src/environment/authentication.js
@@ -4,6 +4,9 @@ import { MODEL__SESSION__MODEL__ENVIRONMENT } from "./models/session.js";
 
 import * as jose from "jose";
 
+// todo: refactor some of this to use higher order functions
+// note: technically it might not make sense to refactor since these are separate auth methods
+
 const VERIFY_JWT_PAYLOAD = function (context, payload) {
 	const session_attributes = {
 		gauze__session__id: payload.session_id,
@@ -77,15 +80,12 @@ const AUTHENTICATE_ENVIRONMENT__AUTHENTICATION__ENVIRONMENT = function (auth) {
 				})
 				.catch(function (err) {
 					// note: maybe log?
-					//return null;
 					throw new Error("Invalid JWT");
 				});
 		} else {
-			//return Promise.resolve(null);
 			return Promise.reject(new Error("Invalid Authorization type"));
 		}
 	} else {
-		//return Promise.resolve(null);
 		return Promise.reject(new Error("Missing authorization header"));
 	}
 };
@@ -131,26 +131,80 @@ const AUTHENTICATE_SYSTEM__AUTHENTICATION__ENVIRONMENT = function (auth) {
 				})
 				.catch(function (err) {
 					// note: maybe log?
-					//return null;
 					throw new Error("Invalid JWT");
 				});
 		} else {
 			return Promise.reject(new Error("Invalid Authorization type"));
-			//return Promise.resolve(null);
 		}
 	} else {
 		return Promise.reject(new Error("Missing authorization header"));
-		//return Promise.resolve(null);
+	}
+};
+
+const DATABASE_JWT_ISSUER = "gauze";
+
+// note: the unwrapped jwt should have an audience that is the resource server
+// note: the wrapped jwt should have an audience that maps to agent_type
+const DATABASE_JWT_AUDIENCE = "database";
+
+// todo: change the algorithm to EdDSA
+// note: will need a master public/private key pair
+// note: we could leave it as symmetric to make it easy to run the serve
+const DATABASE_JWT_HEADER = {
+	alg: "HS256",
+};
+
+const SIGN_DATABASE_JWT__AUTHENTICATION__ENVIRONMENT = function (payload) {
+	const secret = new TextEncoder().encode(process.env.GAUZE_DATABASE_JWT_SECRET);
+	return new jose.SignJWT(payload).setProtectedHeader(DATABASE_JWT_HEADER).setIssuedAt().setIssuer(DATABASE_JWT_ISSUER).setAudience(DATABASE_JWT_AUDIENCE).setExpirationTime("2h").sign(secret);
+};
+
+const VERIFY_DATABASE_JWT__AUTHENTICATION__ENVIRONMENT = function (jwt) {
+	const secret = new TextEncoder().encode(process.env.GAUZE_DATABASE_JWT_SECRET);
+	return jose.jwtVerify(jwt, secret, {
+		issuer: DATABASE_JWT_ISSUER,
+		audience: DATABASE_JWT_AUDIENCE,
+	});
+};
+
+// auth is authentication header
+const AUTHENTICATE_DATABASE__AUTHENTICATION__ENVIRONMENT = function (auth) {
+	// parse the header to extract the token
+	var jwt = null;
+	//const auth = req.headers.authorization;
+	if (auth) {
+		const auth_split = auth.split(" ");
+		if (auth_split[0] === "Bearer") {
+			jwt = auth_split[1];
+			return VERIFY_DATABASE_JWT__AUTHENTICATION__ENVIRONMENT(jwt)
+				.then(function ({ payload }) {
+					return payload;
+				})
+				.catch(function (err) {
+					// note: maybe log?
+					throw new Error("Invalid JWT");
+				});
+		} else {
+			return Promise.reject(new Error("Invalid Authorization type"));
+		}
+	} else {
+		return Promise.reject(new Error("Missing authorization header"));
 	}
 };
 
 export {
 	VERIFY_JWT_PAYLOAD,
 	HASH_PASSWORD__AUTHENTICATION__ENVIRONMENT,
+	// ENVIRONMENT
 	SIGN_ENVIRONMENT_JWT__AUTHENTICATION__ENVIRONMENT,
 	VERIFY_ENVIRONMENT_JWT__AUTHENTICATION__ENVIRONMENT,
 	AUTHENTICATE_ENVIRONMENT__AUTHENTICATION__ENVIRONMENT,
+	// SYSTEM
 	SIGN_SYSTEM_JWT__AUTHENTICATION__ENVIRONMENT,
 	VERIFY_SYSTEM_JWT__AUTHENTICATION__ENVIRONMENT,
 	AUTHENTICATE_SYSTEM__AUTHENTICATION__ENVIRONMENT,
+	// DATABASE
+	SIGN_DATABASE_JWT__AUTHENTICATION__ENVIRONMENT,
+	VERIFY_DATABASE_JWT__AUTHENTICATION__ENVIRONMENT,
+	AUTHENTICATE_DATABASE__AUTHENTICATION__ENVIRONMENT,
 };
