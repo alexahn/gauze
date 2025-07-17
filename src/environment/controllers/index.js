4bb51d3bdaa35557b58b5abe3588ab009080b36c
Alex Ahn <accounts+technology+github@work.alexahn.com>
2025-07-17 15:19:44 -0700

    delete old files

--- src/environment/controllers/index.js
+++ src/environment/controllers/index.js
@@ -1,3 +1,2 @@
 export * as environment from "./environment.js";
-//export * as agent_root from "./agent_root.js";
 export * as agents from "./agents/index.js";
--- src/kernel/src/environment/controllers/agent_root.js
+++
@@ -1,9 +0,0 @@
-import { MODEL__SESSION__MODEL__ENVIRONMENT } from "./../models/session.js";
-
-class AgentRootController {
-	constructor() {}
-}
-
-const CONTROLLER__AGENT_ROOT__CONTROLLER__ENVIRONMENT = new AgentRootController();
-
-export { CONTROLLER__AGENT_ROOT__CONTROLLER__ENVIRONMENT };
