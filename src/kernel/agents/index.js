5123b630a9b49bd7ad236f23e7eeec4e2e4c580b
Alex Ahn <accounts+technology+github@work.alexahn.com>
2025-07-17 08:48:50 -0700

    placeholder for different kernel agent functions

--- src/kernel/agents/index.js
+++ src/kernel/agents/index.js
@@ -1,1 +1,5 @@
-* as accounts from "./accounts.js"
+export * as root from "./root.js"
+export * as account from "./account.js"
+export * as user from "./user.js"
+export * as person from "./person.js"
+export * as character from "./character.js"
