2a03c636696a6ec0e40db71a220ba7614a8b0c9d
Alex Ahn <accounts+technology+github@work.alexahn.com>
2025-07-20 09:09:27 -0700

    add gauze.js to environment realm

---
+++ src/kernel/interfaces/http/router.js
@@ -0,0 +1,32 @@
+import Router from "@koa/router";
+
+import * as $realm from "./../../gauze.js"
+
+export default function ($gauze) {
+	const ROUTER__HTTP__INTERFACE__KERNEL = new Router();
+
+	const database = $gauze.database.knex.create_connection();
+
+	// this is called once the exit trajectory has been set
+	process.on("exit", function (val) {
+		database.destroy();
+	});
+
+	/*
+	ROUTER__HTTP__INTERFACE__SYSTEM.post("/graphql", function (ctx, next) {
+		return $gauze.kernel.src.servers.http.HANDLE_REALM_GRAPHQL__HTTP__SERVER__SRC__KERNEL(
+			{
+				$gauze: $gauze,
+				$realm: $realm,
+				database: database,
+				authenticators: [$gauze.environment.authentication.AUTHENTICATE_SYSTEM__AUTHENTICATION__ENVIRONMENT],
+				schema: $gauze.system.interfaces.graphql.schema.SCHEMA__SCHEMA__GRAPHQL__INTERFACE__SYSTEM,
+			},
+			ctx,
+			next,
+		);
+	});
+	*/
+
+	return ROUTER__HTTP__INTERFACE__KERNEL;
+}
