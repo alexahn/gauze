7094edf66b4c7029c538d386ab4fc80422cac9e8
Alex Ahn <accounts+technology+github@work.alexahn.com>
2025-07-24 10:03:11 -0700

    work in progress designing router

---
+++ src/views/project/src/router.js
@@ -0,0 +1,70 @@
+class Router {
+	constructor() {
+		// routes
+		// current
+	}
+	URLToState() {
+
+	}
+	StateToURL() {
+
+	}
+}
+
+// create routers
+// instantiate routes (will create array structured lookup)
+
+// routes class
+/*
+const router2 = new Router([{
+	name: "world",
+	regex: "/world",
+	string: function (groups) {
+		return "/world"
+	},
+	method: "GET",
+	dependencies: function (ctx) {
+		// ctx is resolved parent dependencies (e.g. ctx.hello.10)
+		return {
+			y: 20
+		}
+	},
+	routes: []
+}])
+
+const router = new Router([{
+	name: "hello",
+	regex: "/hello",
+	string: function (groups) {
+		return "/hello"
+	},
+	method: "GET",
+	dependencies: function (ctx) {
+		return {
+			x: 10
+		}
+	},
+	routes: router2
+}])
+*/
+
+/*
+const director = new Director({
+	
+})
+director.handle("hello.world", function (route, params, dependencies) {
+
+})
+director.handle("hello", function (route, params, dependencies) {
+	redirect(pathfinder.StateToURL("hello.world", params, dependencies)
+})
+*/
+
+/*
+URLtoState()
+StateToURL()
+pathfinder.navigateToURL(URL).then(function (route, params, dependencies) {})
+pathfinder.navigateToState(name, params).then(function (route, params, dependencies) {}
+	director.navigate(route, params, dependencies);
+})
+*/
