a3147c577cb15ea004633fddd79212b59f35f2fd
Alex Ahn <accounts+technology+github@work.alexahn.com>
2025-07-31 20:36:12 -0700

    style proxies list and also show errors

--- src/views/project/src/components/Proxies.jsx
+++ src/views/project/src/components/Proxies.jsx
@@ -1,9 +1,20 @@
 import * as React from "react";
 import { useState } from "react";
+import * as jose from "jose"
 
 function Proxies({ pathfinder, services, proxies, next }) {
 	const { gauze } = services;
+	const [error, setError] = useState("")
 	const [submitProxy, setSubmitProxy] = useState(false);
+	let currentAgent
+
+	const systemJWT = gauze.default.getSystemJWT()
+
+	if (systemJWT) {
+		const decoded = jose.decodeJwt(systemJWT)
+		currentAgent = decoded.agent_type
+	}
+
     const order = {
         gauze__agent_root: 0,
         gauze__agent_account: 1,
@@ -25,21 +36,23 @@ function Proxies({ pathfinder, services, proxies, next }) {
 
 	function handleProxy(proxy) {
 		return function (e) {
+			setError("")
 			setSubmitProxy(true);
 			return gauze.default
 				.enterSystemSession(proxy.attributes)
 				.then(function (session) {
-					console.log("system session", session);
 					gauze.default.setSystemJWT(session.gauze__session__value);
 					setSubmitProxy(false);
 					location.replace(next);
 				})
 				.catch(function (err) {
 					setSubmitProxy(false);
+					setError("Something went wrong!")
 				});
 		};
 	}
 
+	const errorClass = "clouds athelas bgx10 bdx10 cx7 ba br2 bw1 pa1 f6";
     const buttonClass = "clouds w-100 ba bw1 br2 truncate-ns mb1 f6 athelas";
     const classes = {
         gauze__agent_root: `${buttonClass} bgx2 bdx2 cx6 bgx3h bdx3h cx6h`,
@@ -49,19 +62,24 @@ function Proxies({ pathfinder, services, proxies, next }) {
         gauze__agent_character: `${buttonClass} bgx10 bdx10 cx6 bgx9h bdx9h`,
     };
 
+	// todo: disable the button for the agent that is currently active (by parsing the system jwt)
 	return (
 		<div>
 			<hr />
 			{proxies.map(function (proxy) {
+				const isActive = proxy.attributes.gauze__proxy__agent_type === currentAgent
 				return (
                     <form key={proxy.gauze__proxy__id} className="mb0" action={handleProxy(proxy)}>
-                        <button className={classes[proxy.attributes.gauze__proxy__agent_type]} type="submit" disabled={submitProxy}>
+                        <button className={classes[proxy.attributes.gauze__proxy__agent_type]} style={{ opacity: isActive ? "0.25" : "1" }} type="submit" disabled={submitProxy || isActive}>
                             {readable[proxy.attributes.gauze__proxy__agent_type]}
                         </button>
                     </form>
 				)
-				//return <button onClick={handleProxy(proxy)}>{proxy._metadata.id}</button>;
 			})}
+            <div className="h1" align="center">
+                <div style={{ visibility: "hidden" }}>*</div>
+                {error ? <label className={errorClass}>{error}</label> : null}
+            </div>
 		</div>
 	);
 }
