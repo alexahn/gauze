027c0ee61eec564ff62208d2bd3178341eb11f0a
Alex Ahn <accounts+technology+github@work.alexahn.com>
2025-08-01 23:41:16 -0700

    focus working on table rows

--- src/views/project/src/components/Table.jsx
+++ src/views/project/src/components/Table.jsx
@@ -3,18 +3,29 @@ import { useState } from "react";
 
 function Table({ pathfinder, services, header, variables, items }) {
 	const { gauze } = services
-	const buttonClass = "clouds w-100 ba bw1 br2 truncate-ns mb1 f6 athelas bgx2 bdx2 cx6 bgx3h bdx3h cx6h truncate-ns mw4";
+	const cellClass = "relative tooltip-container ba bw1 br2 mb1 bgx2 bdx2 cx6 bgx3h bdx3h cx6h"
+	const itemClass = "athelas f6 clouds w-100 truncate-ns mw4";
+	const tooltipClass = "absolute tooltip athelas f6 dn bgx2 cx6 mw5 w5 top-0 left-0 ba bw1 br2"
 	return (<div>
 		<table className="type-list">
-			<tr>
+			<tr className={cellClass} tabIndex="0">
 				{header.fields.map(function (field) {
-					return (<th className={buttonClass}>{field.name}</th>)
+					return (
+						<th className={cellClass} tabIndex="0">
+							<div className={itemClass}>{field.name}</div>
+							<span className={tooltipClass}>{field.name}</span>
+						</th>)
 				})}
 			</tr>
 			{items.map(function (item) {
 				return (<tr>
 					{header.fields.map(function (field) {
-						return (<td className={buttonClass}>{item.attributes[field.name]}</td>)
+						return (
+							<td className={cellClass} tabIndex="0">
+								<div className={itemClass}>{item.attributes[field.name]}</div>
+								<span className={tooltipClass}>{item.attributes[field.name]}</span>
+							</td>
+						)
 					})}
 				</tr>)
 			})}
