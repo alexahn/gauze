b54a84fd96b1576a7d2370351f0f687ac003d9a5
Alex Ahn <accounts+technology+github@work.alexahn.com>
2025-08-02 23:24:49 -0700

    between filter working

--- src/views/project/src/components/Table.jsx
+++ src/views/project/src/components/Table.jsx
@@ -8,9 +8,9 @@ import Pagination from "./Pagination.jsx"
 
 function Table({ pathfinder, services, header, variables = {}, items, count }) {
 	const { gauze } = services
-	const { where = {} } = variables
-	// todo: we can infer the filter mode based on the structure of variables
-	const [filterMode, setFilterMode] = useState("where_like")
+	// note: infer the filter mode based on the structure of variables
+	const defaultFilterMode = variables.where_like ? "where_like" : (variables.where_between ? "where_between" : "where_like")
+	const [filterMode, setFilterMode] = useState(defaultFilterMode)
 	const [localVariables, setLocalVariables] = useState(variables)
 	const cellClass = "relative tooltip-container ba bw1 br2 mb1 bgx2 bdx2 cx6 bgx3h bdx3h cx6h w100"
 	const itemClass = "athelas f6 clouds w-100 truncate-ns mw5";
@@ -54,9 +54,14 @@ function Table({ pathfinder, services, header, variables = {}, items, count }) {
 		})
 		return url
 	}
+	function handleFilterMode(mode) {
+		return function (e) {
+			setFilterMode(mode)
+		}
+	}
 	// note: we need this function because when the input field is empty, we have where_like: { [field]: "" }, which usually returns an empty array
 	// note: if there are any valid queries with null or empty values in the query fields, we would need to rethink this
-	function stripVariables(variables) {
+	function stripVariables(variables, mode) {
 		const modes = ["where", "where_like"]
 		const array_modes = ["where_between"]
 		const stripped = {
@@ -87,9 +92,20 @@ function Table({ pathfinder, services, header, variables = {}, items, count }) {
 				})
 			}
 		})
+		// note: the last version of filtering was too advanced
+		// note: the new version is very basic, and only the values visible on the screen apply to the filter
+		if (mode === "where_like") {
+			delete stripped.where
+			delete stripped.where_between
+		} else if (mode === "where_between") {
+			delete stripped.where
+			delete stripped.where_like
+		} else {
+			
+		}
 		return stripped
 	}
-	function handleChange(field) {
+	function handleFilterChange(field) {
 		return function (e) {
 			const variables = {
 				...localVariables
@@ -104,14 +120,35 @@ function Table({ pathfinder, services, header, variables = {}, items, count }) {
 			setLocalVariables(variables)
 		}
 	}
-	function handleKeyDown(field) {
+	function handleBetweenFilterChange(field, index) {
+		return function (e) {
+			const variables = {
+				...localVariables
+			}
+			if (variables[filterMode]) {
+				if (variables[filterMode][field]) {
+					variables[filterMode][field][index] = e.target.value
+				} else {
+					variables[filterMode][field] = []
+					variables[filterMode][field][index] = e.target.value
+				}
+			} else {
+				variables[filterMode] = {
+					[field]: []
+				}
+				variables[filterMode][field][index] = e.target.value
+			}
+			setLocalVariables(variables)
+		}
+	}
+	function handleFilterKeyDown(field) {
 		return function (e) {
 			if (e.key === "Enter") {
 				// navigate
 				const state = pathfinder.URLToState(location.href)
 				const url = pathfinder.stateToURL(state.name, state.pathParams, {
 					...state.searchParams,
-					variables: JSON.stringify(stripVariables(localVariables))
+					variables: JSON.stringify(stripVariables(localVariables, filterMode))
 				})
 				navigate(url, {
 					replace: false,
@@ -120,27 +157,99 @@ function Table({ pathfinder, services, header, variables = {}, items, count }) {
 			}
 		}
 	}
-	return (<div>
-		<table className="type-list">
-			<thead>
+	function renderFilters() {
+		if (filterMode === "where_like") {
+			return (
 				<tr>
-					<th><button type="button">Search</button></th>
+					<th className={cellClass} tabIndex="0">
+						<button type="button">Search</button>
+						<span className={tooltipClass}>
+							<button type="button" onClick={handleFilterMode("where_like")}>Search</button>
+							<button type="button" onClick={handleFilterMode("where_between")}>Range</button>
+						</span>
+					</th>
 					{header.fields.map(function (field) {
+						const defaultValue = localVariables[filterMode] ? localVariables[filterMode][field.name] : undefined
 						return (
 							<th key={field.name} className={cellClass} tabIndex="0">
-								<div className={itemClass}>
+								<div key={filterMode} className={itemClass}>
 									<Input
 										defaultMode={true}
 										field={field}
 										className="w-100 mw5"
-										onChange={handleChange(field.name)}
-										onKeyDown={handleKeyDown(field.name)}
-										defaultValue={localVariables[filterMode] ? localVariables[filterMode][field.name] : undefined}
+										onChange={handleFilterChange(field.name)}
+										onKeyDown={handleFilterKeyDown(field.name)}
+										defaultValue={defaultValue}
 									/>
 								</div>
 							</th>)
 					})}
 				</tr>
+			)
+		} else if (filterMode === "where_between") {
+			return (
+				<>
+					<tr>
+						<th className={cellClass} tabIndex="0">
+							<button type="button">Start</button>
+							<span className={tooltipClass}>
+								<button type="button" onClick={handleFilterMode("where_like")}>Search</button>
+								<button type="button" onClick={handleFilterMode("where_between")}>Range</button>
+							</span>
+						</th>
+						{header.fields.map(function (field) {
+							const defaultValue0 = localVariables[filterMode] ? (localVariables[filterMode][field.name] ? localVariables[filterMode][field.name][0] : undefined) : undefined;
+							return (
+								<th key={field.name} className={cellClass} tabIndex="0">
+									<div key={filterMode} className={itemClass}>
+										<Input
+											defaultMode={true}
+											field={field}
+											className="w-100 mw5"
+											onChange={handleBetweenFilterChange(field.name, 0)}
+											onKeyDown={handleFilterKeyDown(field.name)}
+											defaultValue={defaultValue0}
+										/>
+									</div>
+								</th>)
+						})}
+					</tr>
+					<tr>
+						<th className={cellClass} tabIndex="0">
+							<button type="button">End</button>
+							<span className={tooltipClass}>
+								<button type="button" onClick={handleFilterMode("where_like")}>Search</button>
+								<button type="button" onClick={handleFilterMode("where_between")}>Range</button>
+							</span>
+						</th>
+						{header.fields.map(function (field) {
+							const defaultValue1 = localVariables[filterMode] ? (localVariables[filterMode][field.name] ? localVariables[filterMode][field.name][1] : undefined) : undefined;
+							return (
+								<th key={field.name} className={cellClass} tabIndex="0">
+									<div key={filterMode} className={itemClass}>
+										<Input
+											defaultMode={true}
+											field={field}
+											className="w-100 mw5"
+											onChange={handleBetweenFilterChange(field.name, 1)}
+											onKeyDown={handleFilterKeyDown(field.name)}
+											defaultValue={defaultValue1}
+										/>
+									</div>
+								</th>
+							)
+						})}
+					</tr>
+				</>
+			)
+		} else {
+
+		}
+	}
+	return (<div>
+		<table className="type-list">
+			<thead>
+				{renderFilters()}
 				<tr>
 					<th><button type="button">Fields</button></th>
 					{header.fields.map(function (field) {
