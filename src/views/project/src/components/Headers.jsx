63a963a435c9861b530cd72a1e69239ce8a5b4c3
Alex Ahn <accounts+technology+github@work.alexahn.com>
2025-08-14 05:41:42 -0700

    visual identifier for currently selected header

--- src/views/project/src/components/Headers.jsx
+++ src/views/project/src/components/Headers.jsx
@@ -3,15 +3,18 @@ import { useState } from "react";
 
 function Headers({ pathfinder, headers }) {
 	const buttonClass = "clouds w-100 ba bw1 br2 truncate-ns mb1 f6 athelas bgx2 bdx2 cx6 bgx3h bdx3h cx6h truncate-ns mw4";
+	const buttonActiveClass = "clouds w-100 ba bw1 br2 truncate-ns mb1 f6 athelas bgx3 bdx3 cx6 bgx3h bdx3h cx6h truncate-ns mw4";
+	const state = pathfinder.URLToState(location.href)
 	return (
 		<div>
 			{headers.map(function (header) {
 				// note: default variables are where_like and limit 20
 				const variables = JSON.stringify({ where_like: {}, limit: 20 });
+				const isActive = state.pathParams.header === header.graphql_meta_type.toLowerCase()
 				return (
 					<div key={header.name} className="mb0">
 						<a href={pathfinder.stateToURL("project.system.headers.header.list", { header: header.graphql_meta_type.toLowerCase() }, { variables })}>
-							<button className={buttonClass} type="button">
+							<button className={isActive ? buttonActiveClass : buttonClass} type="button">
 								{header.graphql_meta_type}
 							</button>
 						</a>
