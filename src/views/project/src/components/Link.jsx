9b605bee936c204ec99adcc5a424821e149042c4
Alex Ahn <accounts+technology+github@work.alexahn.com>
2025-08-15 04:51:46 -0700

    forgot to add Link component

---
+++ src/views/project/src/components/Link.jsx
@@ -0,0 +1,24 @@
+import * as React from "react";
+
+import { navigate } from "@ahn/sinew"
+
+function Link({ href, push, children }) {
+	function handleClick(e) {
+		if (push) {
+			e.preventDefault()
+			navigate(href, {
+				push: true,
+				replace: false
+			})
+		} else {
+
+		}
+	}
+	return (
+		<a href={href} onClick={handleClick}>
+			{children}
+		</a>
+	);
+}
+
+export default Link;
