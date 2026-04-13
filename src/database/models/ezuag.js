ea88bfaa0c256d24afe77c5a2987444643ae3306
Alex Ahn <accounts+technology+github@work.alexahn.com>
2026-04-13 12:42:43 -0700

    prepare templates to inject database manager into kernel database model

--- src/database/models/ezuag.js
+++ src/database/models/ezuag.js
@@ -2,6 +2,8 @@ import * as $abstract from "./../../abstract/index.js";
 import * as $structure from "./../../structure/index.js";
 import * as $kernel from "./../../kernel/index.js";
 
+import MANAGER__DATABASE from "./../manager.js"
+
 class EzuagEzuagDatabaseModel extends $kernel.src.models.database.DatabaseModel {
 	constructor(root_config, config) {
 		super(root_config, config);
@@ -37,6 +39,6 @@ const CONFIG__EZUAG__MODEL__DATABASE = {
 	table_name: $structure.entities.ezuag.database.sql.TABLE_NAME__SQL__DATABASE__EZUAG__STRUCTURE,
 	primary_key: $structure.entities.ezuag.database.sql.PRIMARY_KEY__SQL__DATABASE__EZUAG__STRUCTURE,
 };
-const MODEL__EZUAG__MODEL__DATABASE = new EzuagEzuagDatabaseModel(ROOT_CONFIG__EZUAG__MODEL__DATABASE, CONFIG__EZUAG__MODEL__DATABASE);
+const MODEL__EZUAG__MODEL__DATABASE = new EzuagEzuagDatabaseModel(ROOT_CONFIG__EZUAG__MODEL__DATABASE, CONFIG__EZUAG__MODEL__DATABASE, MANAGER__DATABASE);
 
 export { MODEL__EZUAG__MODEL__DATABASE };
