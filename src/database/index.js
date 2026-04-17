a6eaa23a1709b7884efbeab3369534ad56670d71
Alex Ahn <accounts+technology+github@work.alexahn.com>
2026-04-17 11:29:10 -0700

    remove old knex.js and knexfile.js

--- src/database/index.js
+++ src/database/index.js
@@ -1,4 +1,3 @@
-export * as knex from "./knex.js";
 export * as controllers from "./controllers/index.js";
 export * as interfaces from "./interfaces/index.js";
 export * as models from "./models/index.js";
