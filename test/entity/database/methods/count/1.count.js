6dc8a9023ef8a205995e1dcfedd40aa594c27108
Alex Ahn <accounts+technology+github@work.alexahn.com>
2026-04-16 21:30:32 -0700

    start adding tests for count method

---
+++ test/entity/database/methods/count/1.count.js
@@ -0,0 +1,41 @@
+const description = `Count a entity object`;
+const operation_name = "CountEntity";
+const operation_source = `
+query ${operation_name}(
+	$where: Entity_Query__Where
+) {
+	count_entity(where: $where) {
+		select
+		count
+	}
+}
+`;
+const operation_variables = {
+	where: {
+		id: "3ab515f4-5391-4af9-a2c9-a7119ad262ce",
+	},
+};
+const expected = `{
+    "data": {
+        "count_entity": [
+            {
+                "select": "count(*)",
+                "count": 1
+            }
+        ]
+    }
+}`
+
+export default {
+	step: 1,
+	description: description,
+	context: {
+		agent_id: "1",
+	},
+	operation: {
+		name: operation_name,
+		source: operation_source,
+		variables: operation_variables,
+	},
+	expected: expected,
+};
