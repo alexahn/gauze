81bec74967ef157591e1b00b8cadce0d52bca981
Alex Ahn <accounts+technology+github@work.alexahn.com>
2026-04-21 10:34:14 -0700

    refactor project router.js to not rely on current working directory

--- src/router.js
+++ src/router.js
@@ -1,6 +1,4 @@
 import path from "path";
-const __RELATIVE_FILEPATH = path.relative(process.cwd(), import.meta.filename);
-const __RELATIVE_DIRECTORY = path.relative(process.cwd(), import.meta.dirname);
 
 import url from "url";
 
@@ -13,6 +11,9 @@ import { send } from "@koa/send";
 import etag from "@koa/etag";
 import compress from "koa-compress";
 import conditional from "koa-conditional-get";
+
+const GAUZE_V1_BUILD_ROOT = path.resolve(import.meta.dirname, "./views/gauze/v1/build");
+const PROJECT_BUILD_ROOT = path.resolve(import.meta.dirname, "./views/project/build");
 
 // note: currently unused (edit src/views/gauze/build/index.html instead)
 function gauzeIndex() {
@@ -72,33 +73,33 @@ export default function ($gauze) {
 	ROUTER.get("/gauze/v1/(.*)", async function (ctx, next) {
 		if (ctx.get("referrer")) {
 			const referrer_parsed = new url.URL(ctx.get("referrer"));
-			const referrer_directory = referrer_parsed.pathname[referrer_parsed.pathname.length - 1] === "/" ? referrer_parsed.pathname : path.dirname(referrer_parsed.pathname);
-			const relative_path = path.relative(referrer_directory, ctx.path);
-			if (path.extname(ctx.path)) {
-				await send(ctx, relative_path, { root: __RELATIVE_DIRECTORY + "/views/gauze/v1/build", index: "index.html" });
+				const referrer_directory = referrer_parsed.pathname[referrer_parsed.pathname.length - 1] === "/" ? referrer_parsed.pathname : path.dirname(referrer_parsed.pathname);
+				const relative_path = path.relative(referrer_directory, ctx.path);
+				if (path.extname(ctx.path)) {
+					await send(ctx, relative_path, { root: GAUZE_V1_BUILD_ROOT, index: "index.html" });
+				} else {
+					//ctx.status = 200;
+					//ctx.body = gauzeIndex();
+					//await next();
+					await send(ctx, "/index.html", { root: GAUZE_V1_BUILD_ROOT, index: "index.html" });
+				}
 			} else {
-				//ctx.status = 200;
-				//ctx.body = gauzeIndex();
-				//await next();
-				await send(ctx, "/index.html", { root: __RELATIVE_DIRECTORY + "/views/gauze/v1/build", index: "index.html" });
+				// remove /gauze prefix from path when accessing files from root directory
+				if (path.extname(ctx.path)) {
+					// note: prefix length is 2 because /gauze/v1 has 2 segments
+					const prefix_length = 2;
+					const path_split = ctx.path.split("/");
+					const rebased_path_split = path_split.slice(0, 1).concat(path_split.slice(prefix_length + 1));
+					const rebased_path = rebased_path_split.join("/");
+					await send(ctx, rebased_path, { root: GAUZE_V1_BUILD_ROOT, index: "index.html" });
+				} else {
+					//ctx.status = 200;
+					//ctx.body = gauzeIndex();
+					//await next();
+					await send(ctx, "/index.html", { root: GAUZE_V1_BUILD_ROOT, index: "index.html" });
+				}
 			}
-		} else {
-			// remove /gauze prefix from path when accessing files from root directory
-			if (path.extname(ctx.path)) {
-				// note: prefix length is 2 because /gauze/v1 has 2 segments
-				const prefix_length = 2;
-				const path_split = ctx.path.split("/");
-				const rebased_path_split = path_split.slice(0, 1).concat(path_split.slice(prefix_length + 1));
-				const rebased_path = rebased_path_split.join("/");
-				await send(ctx, rebased_path, { root: __RELATIVE_DIRECTORY + "/views/gauze/v1/build", index: "index.html" });
-			} else {
-				//ctx.status = 200;
-				//ctx.body = gauzeIndex();
-				//await next();
-				await send(ctx, "/index.html", { root: __RELATIVE_DIRECTORY + "/views/gauze/v1/build", index: "index.html" });
-			}
-		}
-	});
+		});
 
 	ROUTER.get("/project", function (ctx, next) {
 		if (ctx.path[ctx.path.length - 1] === "/") {
@@ -112,32 +113,32 @@ export default function ($gauze) {
 	ROUTER.get("/project/(.*)", async function (ctx, next) {
 		if (ctx.get("referrer")) {
 			const referrer_parsed = new url.URL(ctx.get("referrer"));
-			const referrer_directory = referrer_parsed.pathname[referrer_parsed.pathname.length - 1] === "/" ? referrer_parsed.pathname : path.dirname(referrer_parsed.pathname);
-			const relative_path = path.relative(referrer_directory, ctx.path);
-			if (path.extname(ctx.path)) {
-				await send(ctx, relative_path, { root: __RELATIVE_DIRECTORY + "/views/project/build", index: "index.html" });
+				const referrer_directory = referrer_parsed.pathname[referrer_parsed.pathname.length - 1] === "/" ? referrer_parsed.pathname : path.dirname(referrer_parsed.pathname);
+				const relative_path = path.relative(referrer_directory, ctx.path);
+				if (path.extname(ctx.path)) {
+					await send(ctx, relative_path, { root: PROJECT_BUILD_ROOT, index: "index.html" });
+				} else {
+					//ctx.status = 200;
+					//ctx.body = projectIndex();
+					//await next();
+					await send(ctx, "/index.html", { root: PROJECT_BUILD_ROOT, index: "index.html" });
+				}
 			} else {
-				//ctx.status = 200;
-				//ctx.body = projectIndex();
-				//await next();
-				await send(ctx, "/index.html", { root: __RELATIVE_DIRECTORY + "/views/project/build", index: "index.html" });
+				// remove /project prefix from path when accessing files from root directory
+				if (path.extname(ctx.path)) {
+					const prefix_length = 1;
+					const path_split = ctx.path.split("/");
+					const rebased_path_split = path_split.slice(0, 1).concat(path_split.slice(prefix_length + 1));
+					const rebased_path = rebased_path_split.join("/");
+					await send(ctx, rebased_path, { root: PROJECT_BUILD_ROOT, index: "index.html" });
+				} else {
+					//ctx.status = 200;
+					//ctx.body = projectIndex();
+					//await next();
+					await send(ctx, "/index.html", { root: PROJECT_BUILD_ROOT, index: "index.html" });
+				}
 			}
-		} else {
-			// remove /project prefix from path when accessing files from root directory
-			if (path.extname(ctx.path)) {
-				const prefix_length = 1;
-				const path_split = ctx.path.split("/");
-				const rebased_path_split = path_split.slice(0, 1).concat(path_split.slice(prefix_length + 1));
-				const rebased_path = rebased_path_split.join("/");
-				await send(ctx, rebased_path, { root: __RELATIVE_DIRECTORY + "/views/project/build", index: "index.html" });
-			} else {
-				//ctx.status = 200;
-				//ctx.body = projectIndex();
-				//await next();
-				await send(ctx, "/index.html", { root: __RELATIVE_DIRECTORY + "/views/project/build", index: "index.html" });
-			}
-		}
-	});
+		});
 
 	return ROUTER;
 }
