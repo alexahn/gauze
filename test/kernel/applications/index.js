0773bfad01e8ab51565acfbe953c67abd3d71369
Alex Ahn <accounts+technology+github@work.alexahn.com>
2026-05-04 19:00:33 -0700

    Revert "add tests for cli"
    This reverts commit 9e6b5e8fe780f0015993adea3c5161671942789a.

--- test/kernel/applications/index.js
+++
@@ -1,2 +0,0 @@
-export * as cli from "./cli.js";
-export * as manager from "./manager.js";
--- test/kernel/index.js
+++ test/kernel/index.js
@@ -1,3 +1,2 @@
-export * as applications from "./applications/index.js";
 export * as config from "./config.js";
 export * as models from "./models/index.js";
